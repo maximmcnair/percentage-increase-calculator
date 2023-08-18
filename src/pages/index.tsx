84b0cfb416671664e354badf1b1423eeb8e4e7c9
Maxim McNair <mail@maximmcnair.com>
2023-08-18 11:23:04 +0100

    setup increase calculator

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,162 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useEffect, useState } from "react";
+import { Inter } from "next/font/google";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ClipboardCopyIcon, CheckIcon } from "@radix-ui/react-icons";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [startingValue, setStartingValue] = useState(0);
+  const [endingValue, setEndingValue] = useState(0);
+
+  const [percentageChange, setPercentageChange] = useState<number | null>(null);
+  const [multiplier, setMultiplier] = useState<number | null>(null);
+
+  const [percentageChangeCopied, setPercentageChangeCopied] = useState(false);
+  const [percentageChangeCopyShow, setPercentageChangeCopyShow] =
+    useState(false);
+  const [multiplierCopied, setMultiplierCopied] = useState(false);
+  const [multiplierCopyShow, setMultiplierCopyShow] = useState(false);
+
+  function copyPercentageChange() {
+    setPercentageChangeCopied(true);
+    navigator.clipboard.writeText(String(percentageChange) + "%");
+
+    setTimeout(() => {
+      setPercentageChangeCopied(false);
+    }, 1000);
+  }
+
+  function copyMultiplier() {
+    setMultiplierCopied(true);
+    navigator.clipboard.writeText(String(multiplier) + "x");
+
+    setTimeout(() => {
+      setMultiplierCopied(false);
+    }, 1000);
+  }
+
+  useEffect(() => {
+    const change = ((endingValue - startingValue) / startingValue) * 100;
+    setPercentageChange(change);
+
+    if (change === null) return;
+
+    if (change < 0) {
+      setMultiplier(1 - Math.abs(change / 100));
+    } else {
+      setMultiplier(1 + change / 100);
+    }
+  }, [startingValue, endingValue]);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <TooltipProvider>
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      >
+        <div>
+          <h1 className="text-center mb-7 text-3xl text-extrabold">
+            Percentage Increase Calculator
+          </h1>
+          <div className="grid grid-cols-2 gap-4">
+            <label className="bg-zinc-900 p-2 rounded">
+              <strong className="block mb-2 text-center">Starting Value</strong>
+              <Input
+                className="text-center bg-zinc-950"
+                style={{ paddingTop: 3, minHeight: 47 }}
+                value={startingValue}
+                type="number"
+                onChange={(evt) =>
+                  setStartingValue(parseFloat(evt.target.value))
+                }
+              />
+            </label>
+
+            <label className="bg-zinc-900 p-2 rounded">
+              <strong className="block mb-2 text-center">Ending Value</strong>
+              <Input
+                className="text-center bg-zinc-950"
+                style={{ paddingTop: 3, minHeight: 47 }}
+                value={endingValue}
+                type="number"
+                onChange={(evt) => setEndingValue(parseFloat(evt.target.value))}
+              />
+            </label>
+
+            <div className="bg-zinc-900 p-2 rounded">
+              <span
+                className="block text-center bg-zinc-950 mb-2 px-3 py-1 border rounded-md cursor-pointer hover:border-white relative"
+                style={{ paddingTop: 10, minHeight: 47 }}
+                onClick={copyPercentageChange}
+                onMouseEnter={() => {
+                  setPercentageChangeCopyShow(true);
+                }}
+                onMouseLeave={() => {
+                  setPercentageChangeCopyShow(false);
+                }}
+              >
+                {percentageChange === null ||
+                isNaN(percentageChange) ||
+                multiplier === Infinity
+                  ? "-"
+                  : +percentageChange.toFixed(2) + "%"}
+
+                {percentageChangeCopyShow ? (
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="absolute right-1 top-1"
+                  >
+                    {percentageChangeCopied ? (
+                      <CheckIcon className="h-4 w-4" />
+                    ) : (
+                      <ClipboardCopyIcon className="h-4 w-4" />
+                    )}
+                  </Button>
+                ) : null}
+              </span>
+              <strong className="block text-center">Percentage Change</strong>
+            </div>
+
+            <div className="bg-zinc-900 p-2 rounded">
+              <span
+                className="block text-center bg-zinc-950 mb-2 px-3 py-1 border rounded-md cursor-pointer hover:border-white relative"
+                style={{ paddingTop: 10, minHeight: 47 }}
+                onClick={copyMultiplier}
+                onMouseEnter={() => {
+                  setMultiplierCopyShow(true);
+                }}
+                onMouseLeave={() => {
+                  setMultiplierCopyShow(false);
+                }}
+              >
+                {multiplier === null ||
+                isNaN(multiplier) ||
+                multiplier === Infinity
+                  ? "-"
+                  : +multiplier.toFixed(2) + "x"}
+
+                {multiplierCopyShow ? (
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="absolute right-1 top-1"
+                  >
+                    {multiplierCopied ? (
+                      <CheckIcon className="h-4 w-4" />
+                    ) : (
+                      <ClipboardCopyIcon className="h-4 w-4" />
+                    )}
+                  </Button>
+                ) : null}
+              </span>
+              <strong className="block text-center">Multiplier</strong>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </main>
+    </TooltipProvider>
+  );
 }
